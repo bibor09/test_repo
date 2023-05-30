01bc2b79dd8dc93cc5cf88b9d1c897c1251d0ac3
adorjani.biborka <adorjani.biborka@gmail.com>
2023-05-30 17:28:07 +0300

    Add sample2

---
+++ test1/sample2.py
@@ -0,0 +1,46 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        pass
+
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+    def fetch(self):
+        return "Fetching a ball."
+
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+    def scratch(self):
+        return "Scratching the furniture."
+
+
+class Bird(Animal):
+    def speak(self):
+        return "Chirp!"
+
+    def fly(self):
+        return "Flying in the sky."
+
+
+dog = Dog("Buddy")
+print(dog.name)  # Output: Buddy
+print(dog.speak())  # Output: Woof!
+print(dog.fetch())  # Output: Fetching a ball.
+
+cat = Cat("Whiskers")
+print(cat.name)  # Output: Whiskers
+print(cat.speak())  # Output: Meow!
+print(cat.scratch())  # Output: Scratching the furniture.
+
+bird = Bird("Tweetie")
+print(bird.name)  # Output: Tweetie
+print(bird.speak())  # Output: Chirp!
+print(bird.fly())  # Output: Flying in the sky.
